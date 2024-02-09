6da530f97c4855379be200f38cbc9c04196e881d
MaiKhuu1993 <khuu.tuyetmai@gmail.com>
2024-02-08 21:05:08 -0500

    forms and flash messages

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,21 @@
 class Event < ApplicationRecord
+  
+  validates :name, :location, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :capacity, numericality: { 
+    only_integers: true, 
+    greater_than: 0
+  }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :image_file_name, format: {
+    with: /\w+\.(png|jpg)\z/i,
+    message: "must be JPG or PNG"
+  }
+
+  def self.upcoming
+    where("starts_at > ?", Time.now).order("starts_at")
+  end
+  
   def free?
     price.zero? || price.blank?
   end
